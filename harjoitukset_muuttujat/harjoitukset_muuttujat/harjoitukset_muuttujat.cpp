93410f19ed3fbcec5db2565497872d78884cf1c5
Lucas Kajander <lucas.kajander@ICTLAB.local>
2026-09-11 13:55:29 +0300

    Työ

---
+++ harjoitukset_muuttujat/harjoitukset_muuttujat/harjoitukset_muuttujat.cpp
@@ -0,0 +1,55 @@
+// harjoitukset_muuttujat.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <string>
+using namespace std;
+
+int intNum = 5;
+float floatNum = 5.3f;
+double doubleNum = 10.1;
+float sum;
+
+int num = 150,  num2 = 300,  tempNum;
+
+string word1 = "My";  string word2 = "own"; string word3 = "hello"; string word4 = "world!";
+
+
+
+
+
+
+
+
+/*void SwitchValues()
+{
+    tempNum = num;
+    num = num2;
+    num2 = tempNum;
+}*/
+    void TuntiTeht5()
+    {
+        cout << "If this text" << " appears on your display," << endl << "you can pat yourself on " << " the back!" << endl;
+
+        
+           
+    }
+
+int main()
+{
+   // sum = intNum + floatNum + doubleNum;
+   // std::cout << sum;
+
+   // SwitchValues();
+    //    std::cout << num2;
+
+
+    //cout << word1 << word2 << word3 << word4 << endl;
+
+    //cout << "Haiku \"October's Gold\" By Paul Holmes" << endl << endl << "Like crunchy cornflakes" << endl << "Gold leaves rustle underfoot" << endl << "Beauty in decay.";
+
+    TuntiTeht5();
+}
+
+
